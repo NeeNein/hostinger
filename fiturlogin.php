648fdd52690fb1b3a629d0904d1e612619754e56
NeeNein <basithmaster@gmail.com>
2023-07-27 04:26:48 +0700

    Update

--- fiturlogin.php
+++ fiturlogin.php
@@ -4,34 +4,28 @@ $email = $_POST['email'];
 $password = $_POST['password'];
 
 // Menghubungkan ke server database (sesuaikan dengan konfigurasi server Anda)
-$servername = 'localhost';
-$username_db = 'root';
-$password_db = '';
-$database = "nusantara";
-
-$conn = new mysqli($servername, $username_db, $password_db, $database);
-
+include './php/koneksi.php';
 // Memeriksa koneksi
-if ($conn->connect_error) {
-    die("Koneksi gagal: " . $conn->connect_error);
+if ($koneksi->connect_error) {
+    die("Koneksi gagal: " . $koneksi->connect_error);
 }
 
 // Mengamankan data yang akan digunakan dalam query SQL
-$secured_email = $conn->real_escape_string($email);
-$secured_password = $conn->real_escape_string($password);
+$secured_email = $koneksi->real_escape_string($email);
+$secured_password = $koneksi->real_escape_string($password);
 
 // Mengecek kecocokan email, username, dan password di database
 $sql = "SELECT * FROM users WHERE email = '$secured_email'  AND password = '$secured_password'";
-$result = $conn->query($sql);
+$result = $koneksi->query($sql);
 
 if ($result->num_rows > 0) {
     // Jika ada data yang cocok, berarti login berhasil
     echo "Login berhasil!";
-    header("Location: ./php/index.php"); // redirect to index.html
+    header("Location: ./php/bukukita.php"); // redirect to index.html
 } else {
     // Jika tidak ada data yang cocok, berarti login gagal
     echo "Email, username, atau password salah.";
 }
 
-$conn->close();
+$koneksi->close();
 ?>
