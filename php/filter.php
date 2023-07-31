578ba858daea66dc9daa9c1fb3c4198cceaf2b24
NeeNein <basithmaster@gmail.com>
2023-07-31 22:22:53 +0700

    hehe

--- php/filter.php
+++ php/filter.php
@@ -19,20 +19,35 @@ $result = $koneksi->query($sql);
 <body>
     <h1>Hasil Pencarian untuk Kategori: <?php echo $kategori; ?></h1>
     <ul>
-        <?php
-        if ($result->num_rows > 0) {
-            while ($row = $result->fetch_assoc()) {
-                echo "<li>" . $row["title"] . "</li>";
-            }
-        } else {
-            echo "Tidak ada buku dalam kategori ini.";
-        }
-        ?>
-    </ul>
-</body>
-</html>
-
+        <!-- pencarian.php -->
 <?php
-// Tutup koneksi database
-$koneksi->close();
+// Koneksi ke database
+include 'koneksi.php';
+
+// Periksa apakah ada parameter kategori yang dikirimkan
+if (isset($_GET['kategori'])) {
+    $kategori = $_GET['kategori'];
+    $displayedTitles = array(); // Array to store displayed titles
+
+    // Query ke database untuk mencari data dari tabel "books" berdasarkan kategori
+    $query_books = "SELECT id, title, author, price, image FROM books WHERE kategori = '$kategori'";
+    $result_books = mysqli_query($koneksi, $query_books);
+
+    // Tampilkan hasil pencarian
+    if (mysqli_num_rows($result_books) > 0) {
+        while ($row = mysqli_fetch_assoc($result_books)) {
+            echo '<div class="result-item">';
+            echo '<br>';
+            $imagePath = '../' . $row['image']; // Menambahkan "../" sebelum path gambar
+            echo '<a href="detail_buku.php?id=' . $row['id'] . '"><img src="' . $imagePath . '" alt="Book Image"></a>';
+            echo '<h1 class="tolol"> ' . $row['title'] . '</h1>';
+            echo '<h2>' . $row['author'] . '</h2>';
+            echo '<p>Rp. ' . number_format($row['price'], 0, ',', '.') . '</p>';
+            echo '</div>';
+        }
+    } else {
+        echo '<p class="no-result">Tidak ada hasil yang ditemukan.</p>';
+    }
+}
 ?>
+
